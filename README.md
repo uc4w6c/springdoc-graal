1a1f04b667d51654f09f17934fa3f02f3b81fa77
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-06-03 17:48:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## 作成と実行
+$ gradle nativeCompile
+$ ./build/native/nativeCompile/springdoc-graal
---
+++ src/main/java/com/example/springdocgraal/SpringdocGraalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocgraal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocGraalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocGraalApplication.class, args);
+	}
+
+}
